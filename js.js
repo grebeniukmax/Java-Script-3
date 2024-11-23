a9d5415dfa2315b12e1fdbe2e4811ca6bc4811a0
гребенюкмакс <148119706+grebeniukmax@users.noreply.github.com>
2024-11-23 15:54:10 +0200

    Java-Script-3

--- js.js
+++ js.js
@@ -1,4 +1,19 @@
 // Перше //
+let result = 5 + 5 + '5';
+console.log(result);
 // Друге //
+let email = "grmax000@gmail.com";
+let emailChacking = email.includes('@');
+let emailEmail = email.length;
+console.log(emailChacking)
+console.log(emailEmail)
 // Трете //
+const firstName = "My";
+const secondName = "name";
+const thirdName = "is";
+const fullName = firstName + secondName + thirdName + "Viktor" // Але як зробити відступи? //
+console.log(fullName);
 // Четверте //
+let userName = 'Олександре';
+let payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
